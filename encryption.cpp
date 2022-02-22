049b6ceab2bbb43ed9086a7fc0cae7602e5fff1b
oops-aman <b200054@nitsikkim.ac.in>
2022-02-22 08:50:52 +0530

    Encryption

---
+++ encryption.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    string s;
+    cin >> s;
+    auto n = s.size();
+    int row = round(sqrt(n));
+    int column;
+    if (row >= sqrt(n)) column = row; else column = row + 1;
+    for(int j=0;j<column;++j) {
+        for(int i=j; i<n;i+=column)cout << s[i];
+        cout << ' ';
+    }
+    return 0;
+}
