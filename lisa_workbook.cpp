6d0d91dcd9d37a0f06bd7e54158159b0bee379fa
oops-aman <b200054@nitsikkim.ac.in>
2022-03-09 08:55:13 +0530

    Lisa Workbook

---
+++ lisa_workbook.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int numProblems, specialProblems = 0, pageNumber = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> numProblems;
+        pageNumber++;
+        int problem = 1;
+        while(numProblems > 0) {
+            numProblems --;
+            if(problem == pageNumber) {
+                specialProblems++;
+            }
+            if(problem % k == 0 && numProblems != 0) {
+                pageNumber++;
+            }
+            problem++;
+        }
+    }
+    cout << specialProblems << endl;
+}
