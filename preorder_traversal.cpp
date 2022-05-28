2f0bf7b8da840738ea97174b5543ada8cee14968
oops-aman <b200054@nitsikkim.ac.in>
2022-05-28 23:39:37 +0530

    Tree: Preorder Traversal

---
+++ preorder_traversal.cpp
@@ -0,0 +1,30 @@
+
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void preOrder(Node *root) {
+        if(root == 0)
+            return;
+        else {
+            std::cout << root -> data << " ";
+            preOrder(root -> left);
+            preOrder(root -> right);
+            
+        }
+    }
+
