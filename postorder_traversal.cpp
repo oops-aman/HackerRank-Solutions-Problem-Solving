989890b3e21169740ff559f6e634bdc7421215a0
oops-aman <b200054@nitsikkim.ac.in>
2022-05-28 23:40:06 +0530

    Tree: Postorder Traversal

---
+++ postorder_traversal.cpp
@@ -0,0 +1,30 @@
+
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+
+    void postOrder(Node *root) {
+        if(root == 0)
+            return ;
+        else {
+            postOrder(root -> left);
+            postOrder(root -> right);
+            std::cout << root -> data << " ";
+        }
+    }
+
