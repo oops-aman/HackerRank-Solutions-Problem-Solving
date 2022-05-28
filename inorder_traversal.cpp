b89918d52096243ee5a6d13dd7158bd3476f232f
oops-aman <b200054@nitsikkim.ac.in>
2022-05-28 23:40:27 +0530

    Inorder Traversal

---
+++ inorder_traversal.cpp
@@ -0,0 +1,30 @@
+
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void inOrder(Node *root) {
+        if(root == 0)
+            return ;
+        else {
+            inOrder(root -> left);
+            std::cout << root -> data << " ";
+            inOrder(root -> right);
+            
+        }
+    }
+
